e78ddfa7c76acc94efefdab9ee8dffdcbbdddcb5
= <=>
2011-12-17 03:17:43 +0900

    Fixed issues with HelloWorld Plugin

--- HelloWorld/HelloWorld.php
+++ HelloWorld/HelloWorld.php
@@ -13,17 +13,17 @@ class HelloWorld
 	{
 		global $flamedesk;
 		//Registers HelloWorld template
-		$flamedesk->template["HelloWorld"] = "<b>Helo World</b>";
+		$flamedesk->template["helloworld"] = "<b>Hello World</b>";
 		//Adds the HelloWorld template to the top of the Header template
-		$flamedesk->template["Header"] = "{$flamedesk->template["HelloWorld"]}\n".$flamedesk->template["Header"];
+		$flamedesk->template["header"] = "{$flamedesk->template[\"helloworld\"]}\n".$flamedesk->template["header"];
 	}
 	public function Deactivate()
 	{
 		global $flamedesk;
 		//Removes HelloWorld template
-		$flamedesk->template["HelloWorld"]->remove();
+		$flamedesk->template["helloworld"]->remove();
 		//searches and replaces in the header
-		$flamedesk->template["HelloWorld"]->replace("{$flamedesk->template["HelloWorld"]}", "");
+		$flamedesk->template["helloworld"]->replace("{$flamedesk->template[\"helloworld\"]}", "");
 	}
 }
 ?>
